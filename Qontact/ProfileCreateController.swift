7062d2a501f08da03823807801e30300d3ffed4f
Rohin Chandra <rohin.chandra@menloschool.org>
2018-11-30 00:36:06 -0500

    Now committing scan and storyboard changes

---
+++ Qontact/ProfileCreateController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileCreateController.swift
+//  Qontact
+//
+//  Created by Rob chandra on 11/30/18.
+//  Copyright © 2018 Rohin chandra. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCreateController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Qontact/ScanController.swift
@@ -0,0 +1,34 @@
+//
+//  ScanController.swift
+//  Qontact
+//
+//  Created by Rob chandra on 11/30/18.
+//  Copyright © 2018 Rohin chandra. All rights reserved.
+//
+
+import UIKit
+
+class ScanController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func SaveButton(_ sender: UIButton) {
+        print("test")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
