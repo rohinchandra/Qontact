75e425428ae65dde7c9bc848946ec5859247c74f
zdearing89 <zdearing@jd20.law.harvard.edu>
2018-11-30 01:11:32 -0500

    Added contact saving

--- Qontact/ScanController.swift
+++ Qontact/ScanController.swift
@@ -8,6 +8,25 @@
 
 import UIKit
 
+import Contacts
+
+// this is a separate helper class
+public final class ContactAuthorizer{
+    public class func authorizeContacts(completionHandler
+        : @escaping (_ succeeded: Bool) -> Void){
+        switch CNContactStore.authorizationStatus(for: .contacts){
+        case .authorized:
+            completionHandler(true)
+        case .notDetermined:
+            CNContactStore().requestAccess(for: .contacts){succeeded, err in
+                completionHandler(err == nil && succeeded)
+            }
+        default:
+            completionHandler(false)
+        }
+    }
+}
+
 class ScanController: UIViewController {
 
     override func viewDidLoad() {
@@ -18,17 +37,33 @@ class ScanController: UIViewController {
     
     @IBAction func SaveButton(_ sender: UIButton) {
         print("test")
+        ContactAuthorizer.authorizeContacts {succeeded in
+            if succeeded{
+                self.createContact()
+            } else{
+                print("Not handled")
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func createContact() {
+        var store = CNContactStore()
+        
+        let fooBar = CNMutableContact()
+        fooBar.givenName = "Foo"
+        fooBar.middleName = "A."
+        fooBar.familyName = "Bar"
+        fooBar.nickname = "Fooboo"
+        
+        // saving the contact to the contact store
+        let request = CNSaveRequest()
+        request.add(fooBar, toContainerWithIdentifier: nil)
+        do{
+            try store.execute(request)
+            print("Successfully stored the contact")
+        } catch let err{
+            print("Failed to save the contact. \(err)")
+        }
     }
-    */
 
 }
